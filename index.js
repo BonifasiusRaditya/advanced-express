8342ae9b196c7e99ab4dd6e67aa0006e8e62e884
Bonifasius Raditya Pandu Hendrianto <149129138+BonifasiusRaditya@users.noreply.github.com>
2025-05-01 15:59:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: "*", 
+    origin: ["https://os.netlabdte.com"],
     methods: ["GET", "POST", "PUT", "DELETE"], 
     credentials: true,  
     allowedHeaders: ["Content-Type", "Authorization"],  
